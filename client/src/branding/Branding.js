0c490d916ec90394a1e39ed997253663d886c522
SeanStiny <>
2022-05-14 21:57:17 +0100

    Hide "SeanStiny" name from overlay

--- client/src/branding/Branding.js
+++ client/src/branding/Branding.js
@@ -1,20 +1,10 @@
 import Avatar from './Avatar'
-import Name from './Name';
 import './Branding.css';
-import twitchIcon from './TwitchGlitchWhite.png';
-import twitterIcon from './twitter-icon-white.png';
 
 export function Branding() {
   return (
     <div className="Branding">
       <Avatar />
-
-      <div className="float">
-        <img className="twitch drop-shadow" src={twitchIcon} alt="" />
-        <img className="twitter drop-shadow" src={twitterIcon} alt="" />
-      </div>
-
-      <Name />
     </div>
   );
 }
